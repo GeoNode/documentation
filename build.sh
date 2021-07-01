5704e9ed6cd9d872a03d4f026dd69735dc30970a
afabiani <alessio.fabiani@geo-solutions.it>
2021-07-01 11:53:39 +0200

    Updating Advanced installation steps
    (cherry picked from commit e8e0cb8531f37c7519727350bbb0f88ad169d435)

--- build.sh
+++ build.sh
@@ -1,1 +1,14 @@
-sphinx-build -T -E -d _build/doctrees-readthedocs -D language=en . _build/html
+#!/bin/bash
+set -e
+
+if [ "$1" != "" ]; then
+    printf "Building Docs for Languge '$1'...\n\n"
+    LANG="$1"
+else
+    printf "Languge not specified, using the default one 'en'...\n\n"
+    LANG="en"
+fi
+
+sphinx-build -b gettext . _build/gettext
+sphinx-intl update -p _build/gettext -l $LANG
+sphinx-build -T -E -d _build/doctrees-readthedocs -D language=$LANG . _build/html/$LANG
